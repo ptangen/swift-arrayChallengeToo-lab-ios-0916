5df932181a3212a2b69f84b5deb2777751d5218d
Paul Tangen <ptangen@ptangen.com>
2016-09-08 14:25:04 -0400

    Complete the array challenge 2.

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -11,16 +11,60 @@ import UIKit
 class ViewController: UIViewController {
     
     // This represents our deliLine.
-    var deliLine: [String] = []
+    var deliLine: [String] = [] // ["Rob", "Catherine", "Paul", "Dom"] //[] // "Albert Einstein", "Cher", "Neil deGrasse Tyson", "Yoshi"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newCustomer = "Bob"
+        print(addNameToDeliLine(newCustomer))
+        print(nowServing())
+        print(deliLineDescription())
+        
+    }
+    
+    func addNameToDeliLine(name:String) -> String {
+        var results = ""
+        
+        if deliLine.isEmpty{
+            results = "Welcome \(name), you're first in line!";
+            deliLine.append(name)
+        } else if name == "Billy Crystal"{
+            results = "Welcome Billy! You can sit wherever you like."
+            deliLine.insert(name, atIndex: 0)
+        } else if name == "Meg Ryan"{
+            results = "Welcome Meg! You can sit wherever you like."
+            deliLine.insert(name, atIndex: 0)
+        } else {
+            results = "Welcome \(name), you're number \(deliLine.count + 1) in line."
+            deliLine.append(name)
+        }
+        
+        return results
+    }
+    
+    func nowServing() -> String {
+        var results = ""
+        if deliLine.isEmpty{
+            results = "There is no one to be served."
+        } else {
+            results = "Now serving \(deliLine[0])!"
+            deliLine.removeAtIndex(0)
+        }
+        return results
+    }
+    
+    func deliLineDescription() -> String {
+        var description = ""
+        if deliLine.isEmpty{
+            description = "The line is currently empty."
+        } else {
+            description = "The line is:"
+            for (index, customer) in deliLine.enumerate() {
+                description = "\(description)\n\(index+1). \(customer)"
+            }
+        }
+        return description
     }
 
-    
-    
-    // Create your methods here
-
-
 }
-
